fd060a0dff05ec1a89169fda45f7b82ff845486b
Simon Watson <s.watson@kainos.com>
2015-06-18 14:23:21 +0100

    Adding database connector.

---
+++ HelloProject/src/app/DbConnector.java
@@ -0,0 +1,35 @@
+package app;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnector {
+	
+	private static Connection connection;
+	
+	private DbConnector() {
+		
+	}
+	
+	public static Connection getInstance() {
+		if(connection == null) {
+			connection = createConnection();
+			return connection;
+		} else {
+			return connection;
+		}
+	}
+
+	private static Connection createConnection() {
+		try {
+			Class<?> driver = Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/employees", "employees_user", "password");
+			return connection;
+		} catch(Exception e) {
+			System.out.println("There was a problem connecting to the database, please contact your sysadmin.");
+			System.exit(1);
+			return null;
+		}
+	}
+}
