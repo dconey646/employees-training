9e356e12c57ae8f5c1af58488ba2371e722fec39
Damien Coney <dconey646@gmail.com>
2015-06-18 14:45:50 +0100

    Updating mysqldump file to include some dummy data

--- employees.sql
+++ employees.sql
@@ -53,7 +53,7 @@ CREATE TABLE `employee` (
   `bonus` decimal(11,2) DEFAULT NULL,
   `pay` decimal(11,2) DEFAULT NULL,
   PRIMARY KEY (`number`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,6 +62,7 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,20000.00,'John Smith',5000.00,2000.00);
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -74,4 +75,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-06-18 13:01:44
+-- Dump completed on 2015-06-18 14:43:49
